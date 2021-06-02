45551f9c5a0d45ea85829f627c28c66cd838f1bc
ericksonder <ericksonder017@gmail.com>
2021-06-02 04:15:28 -0500

    Funcionalidad del componente Search, lista -> - Permite realizar una busqueda en la API (open weather map) - Renderiza la data en el componente Weather

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Search from './components/search/Search'
 import './App.css'
 import 'boxicons'
+// import Cities from './components/cities/Cities'
 
 const App = () => {
   return (
     <div>
       <Search/>
+      {/* <Cities/> */}
+      
     </div>
   )
 }
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,15 +1,53 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Weather from "../weather/Weather";
 import "./searchStyles.css";
+import {getClimaPorCiudad} from '../../services/servicios';
 
 const Search = () => {
+
+  const [busqueda, setBusqueda] = useState('')
+  const [ciudad, setCiudad] = useState('');
+  const [pronosticos, setPronosticos] = useState(undefined);
+
+  // console.log(pronosticos);
+  // useEffect(()=>{
+  //   if (ciudad === '') {
+  //     return
+  //   }
+  //   getClimaPorCiudad(ciudad).then((data) => {
+  //     console.log(data);
+  //     setPronosticos(data);
+  //   })
+
+  // },[ciudad]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // setCiudad(busqueda.trim());
+    getClimaPorCiudad(busqueda.trim()).then(data=> {
+      setPronosticos(data);
+    })
+  }
+
+  const handleChange = (e) => {
+    setBusqueda(e.target.value);
+  }
+
+
   return (
     <div className="searchComponent">
       <h2>Encuentra una ciudad</h2>
-      <form id="searchForm">
+      <form id="searchForm" onSubmit={handleSubmit}>
         <div className="input-container">
-          <input type="text" placeholder="Busca tu ciudad" />
+          <input type="text" placeholder="Busca tu ciudad" 
+          value={busqueda}
+          onChange={handleChange}  />
         </div>
       </form>
+      {
+        pronosticos && <Weather pronosticos={pronosticos}/>
+        
+      }
     </div>
   );
 };
